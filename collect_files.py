beff44d7153e6b178aff013b049192db7c17a497
Vitaliy <vitaliyalyshov@gmail.com>
2025-04-28 16:09:00 +0300

    9 trst

--- collect_files.py
+++ collect_files.py
@@ -22,8 +22,12 @@ for current_dir, subdirs, file_names in os.walk(start_dir):
     depth = len(Path(current_dir).relative_to(start_dir).parts)
     rel = Path(current_dir).relative_to(start_dir)
     parts = rel.parts
-    if max_depth is not None and len(parts) > max_depth:
-        parts = parts[:max_depth]  
+    if max_depth is not None:
+        if depth >= max_depth:
+            subdirs.clear()  
+        if len(parts) >= max_depth:     
+            parts = parts[:max_depth]
+
     new_fold = dist_dir.joinpath(*parts)
     new_fold.mkdir(parents=True, exist_ok=True)
 
